0e88b3e947963bc8664056202e4c7f3dac8adcba
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-14 22:48:06 +0530

    Added fragments

--- app/src/main/java/com/yashraj/skiller/DashboardActivity.java
+++ app/src/main/java/com/yashraj/skiller/DashboardActivity.java
@@ -1,14 +1,49 @@
 package com.yashraj.skiller;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
+    BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.dash_app_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.dash_app_category:
+                    selectedFragment = new CategoryFragment();
+                    break;
+
+                case R.id.dash_app_notification:
+                    selectedFragment = new NotificationsFragment();
+                    break;
+
+                case R.id.dash_app_profile:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activtiy_dashboard);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+
 }
